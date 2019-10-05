a4fd02490eab9e0e304a12fc5f8cc4f47b0d6c14
Hawk of the Death <10281615+ITALIA195@users.noreply.github.com>
2019-10-05 16:21:09 +0200

    Improved code quality

---
+++ Alice.Discord/Core.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alice.Discord
+{
+    public class Core
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync(string token)
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            _client.Log += Log;
+            
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await _client.SetGameAsync("Devoloper Build");
+        }
+
+        private static async Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+        }
+
+        private async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += OnMessageReceived;
+            _client.ReactionAdded += OnReactionAdded;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+        
+        private async Task OnHentaiSwitchPage(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reactions)
+        {
+            var f = Modules.BotUtility.interactiveMessages;
+            for (int i = 0; i < f.Count; i++)
+            {
+                if (f[i].RequestAuthor == reactions.UserId)
+                {
+                    f[i].currentPage = f[i].currentPage+=1;
+                    if (f[i].currentPage <= f[i].ImageCount)
+                    {
+                        var c1 = _client.GetChannel(channel.Id);
+                        var c = (c1 as IMessageChannel);
+                        var m = c.GetMessageAsync(message.Id);
+                        EmbedBuilder eb = new EmbedBuilder();
+                        Console.WriteLine(($"{f[i].path}/{f[i].currentPage}.jpg"));
+                        eb.WithImageUrl(($"{f[i].path}/{f[i].currentPage}.jpg"));
+                        await ((IUserMessage) m.Result).ModifyAsync(msg => msg.Embed = eb.Build());
+                        //(channel as IMessageChannel).SendMessageAsync(f[i].path+"/" + f[i].currentPage + ".jpg");
+                    }
+                }
+            }
+        }
+        
+        private async Task OnReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        {
+            if (_client.GetUser(arg3.UserId).IsBot)
+                return;
+            
+            await OnHentaiSwitchPage(arg1, arg2, arg3);
+        }
+
+        public static string[] cur = new string[]
+        {
+            "Fuck you",
+            "Shut up",
+            "Die already",
+            "I will kill you while u sleep",
+            "Justin Bieber sucks, but you suck just as much Dick",
+            "... Die",
+            "Kill Yourself",
+            "My hate for you is Eternal",
+            "OH SHUT UP ALREADY",
+            "You know... there is this easy trick... take a Toaster... inside a Bath... TO FUCKING KILL YOURSELF",
+            "Death is the only Answer for you",
+            "May i suggest, that you should suck less Dicks?",
+            "Fucking Gaylord",
+            "I am no longer Proud...\n/kill"
+        };
+        
+
+        private async Task OnMessageReceived(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message == null || message.Author.IsBot)
+            {
+                return;
+            }
+            
+#if false
+            await message.Channel.SendMessageAsync(cur[Program.Random.Next(cur.Length)]);
+            return;
+#endif
+
+            if (message.Content == "p!catch alice")
+            {
+                await message.Channel.SendMessageAsync("Do not even try that... I will Bite you.");
+                return;
+            }
+
+            if (message.Content.IndexOf("alice", StringComparison.InvariantCultureIgnoreCase) >= 0)
+            {
+                await message.Channel.SendMessageAsync("no.");
+                return;
+            }
+
+            Console.WriteLine(message.Content);
+            int argPos = 0;
+            if (message.HasStringPrefix("a!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
---
+++ Alice.Discord/Modules/BotUtility.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
+
+namespace Alice.Discord.Modules
+{
+    public class BotUtility : ModuleBase<SocketCommandContext>
+    {
+        public class HentaiContainer
+        {
+            public int ImageCount;
+            public string Name;
+            public ulong id;
+            public int currentPage = 0;
+            public string path;
+            public ulong RequestAuthor;
+        }
+        
+        public static List<HentaiContainer> interactiveMessages = new List<HentaiContainer>();
+        WebClient hentaiClient = new WebClient();
+        
+        [Command("hentai")]
+        private async Task CommandHentai(int id)
+        {
+            const string hentaifoxUrl = "https://hentaifox.com/gallery/{0}";
+            const string coverRegex = @"cover.*?src=""(.*?)"".*?info.*?h1>(.*?)</h1.*?Pages: (\d+)";
+            
+            string webpage = hentaiClient.DownloadString(string.Format(hentaifoxUrl, id));
+            
+            var regex = new Regex(coverRegex, RegexOptions.Singleline);
+            var match = regex.Match(webpage);
+            
+            
+            var embed = new EmbedBuilder();
+            embed.ImageUrl = "https:" + match.Groups[1];
+            embed.Title = match.Groups[2].ToString().Replace("&#39;", "'");
+            embed.Color = Color.Blue;
+
+            int.TryParse(match.Groups[3].Value, out int pages);
+            embed.AddField("Pages", pages);
+            
+            var msg = await Context.Channel.SendMessageAsync(string.Empty, false, embed.Build());
+            
+            var right = new Emoji("▶");
+            await msg.AddReactionAsync(right);
+            string hhh = match.Groups[1].ToString();
+            interactiveMessages.Add(new HentaiContainer()
+            {
+                id = msg.Id,
+                currentPage = 0,
+                Name = match.Groups[2].ToString(),
+                ImageCount = pages,
+                path = "https:"+match.Groups[1].ToString().Substring(0, hhh.LastIndexOf('/')),
+                RequestAuthor = Context.User.Id
+            });
+            for (int i = 0; i < interactiveMessages.Count; i++)
+            {
+                Console.WriteLine(interactiveMessages[i]);
+            }
+        }
+        
+        [Command("hentairdm")]
+        public async Task d()
+        {
+            Random rdm = new Random();
+            await CommandHentai(rdm.Next(50000));
+        }
+        
+        
+        [Command("dbtag")]
+        public async Task getTag(string searchString)
+        {
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString($"https://danbooru.donmai.us/tags.json?search[name_matches]=*{searchString}*&search[order]=count");
+            tagSearch[] t = Newtonsoft.Json.JsonConvert.DeserializeObject<tagSearch[]>(json);
+            string h = string.Empty;
+            for (int i = 0; i < t.Length; i++)
+            {
+                h = h += t[i].name.Replace("_","\\_")+$" ({t[i].post_count})"+"\n";
+                if (i > 5)
+                {
+                    //this is a comment, it shouldnt do anything but for some reason without it the
+                    //program wont run
+                    continue;
+                }
+            }
+            EmbedBuilder b = new EmbedBuilder();
+            b.WithDescription(h);
+            Console.WriteLine(t[0].name);
+            await Context.Channel.SendMessageAsync(string.Empty, false, b.Build());
+        }
+        
+        public class tagSearch
+        {
+            public string name;
+            public string post_count;
+        }
+        
+        [Command("tagunban")]
+        public async Task unbanTag(string tag)
+        {
+            if (Context.User.Id == 168407391317000192)
+            {
+                bannedTags.Remove(tag.ToLowerInvariant());
+                await Context.Channel.SendMessageAsync($"Unbanned Tag: [{tag}]");
+
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You are not an Administrator");
+            }
+        }
+        
+        [Command("8ball")]
+        public async Task ball8([Remainder] string s)
+        {
+            string[] h = { "Yes", "No", "Maybe", "I dont know", "Possibly", "I highly doubt it", "Definetely", "Most likely" };
+            await Context.Channel.SendMessageAsync(h[Program.Random.Next(h.Length)]);
+        }
+        
+		[Command("pat")]
+		public async Task pat(string s)
+        {
+			await Context.Channel.SendMessageAsync($"Patted {s}");
+		}
+        
+        [Command("tagban")]
+        public async Task banTag(string tag)
+        {
+            if (Context.User.Id == 168407391317000192)
+            {
+                bannedTags.Add(tag.ToLowerInvariant());
+                await Context.Channel.SendMessageAsync($"Banned Tag: [{tag}]");
+            }
+            else 
+            {
+                await Context.Channel.SendMessageAsync("You are not an Administrator");
+            }
+        }
+        
+        WebClient wc = new WebClient();
+        static List<string> bannedTags = new List<string>();
+        [Command("db")]
+        public async Task getImage(string tag)
+        {
+            if (bannedTags.Contains(tag.ToLowerInvariant()))
+            {
+                await Context.Channel.SendMessageAsync($"Tag [{tag}] is banned from usage");
+                return;
+            }
+            
+            string jsonString = wc.DownloadString($"https://danbooru.donmai.us/posts.json?tags={tag}&limit=1&random=true");
+
+            var image = JsonConvert.DeserializeObject<List<DBImageGetClass>>(jsonString)[0];
+
+            var eb = new EmbedBuilder();
+            eb.Color = Color.Blue;
+            eb.ImageUrl = image.Url;
+            eb.AddField("Artist:", image.Artist.Replace("_", "\\_"));
+            eb.AddField("Tags:", image.Tags.Replace("_", "\\_"));
+            
+            await Context.Channel.SendMessageAsync(string.Empty, false, eb.Build());
+        }
+        
+        public class DBImageGetClass
+        {
+            [JsonProperty("file_url")]
+            public string Url;
+            
+            [JsonProperty("tag_string")]
+            public string Tags;
+            
+            [JsonProperty("tag_string_artist")]
+            public string Artist;
+        }
+
+    }
+}
